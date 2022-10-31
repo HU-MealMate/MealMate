9dbba4b975a767145ae498619773b47002b2a507
ashaanderson <83426123+ashaanderson@users.noreply.github.com>
2022-10-31 15:24:29 -0400

    Add files via upload

---
+++ src/main/java/Mealmate/FragmentTabbar/CouponFragment.kt
@@ -0,0 +1,18 @@
+package Mealmate.fragmentstabbar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CouponFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_coupons, container, false)
+    }
+}
